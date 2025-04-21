08bb85fc80d5bce3f5da30f1badb61828e0d5216
Keyur Shelke <shelkekeyur@gmail.com>
2025-04-21 18:36:48 +0530

    hero/arrow remove update

--- components/hero.tsx
+++ components/hero.tsx
@@ -86,26 +86,6 @@ export default function Hero() {
           </Link>
         </motion.div>
       </motion.div>
-
-      {/* Animated arrow down */}
-      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="text-primary"
-        >
-          <path
-            d="M12 5V19M12 19L19 12M12 19L5 12"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </div>
     </section>
   );
 }
