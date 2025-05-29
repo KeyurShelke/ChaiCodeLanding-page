bbf03e03df4c3399a492f87410cfb8d1e289e121
Keyur Shelke <shelkekeyur@gmail.com>
2025-05-29 15:38:00 +0530

    cohort video issue solved

--- components/cohorts-section.tsx
+++ components/cohorts-section.tsx
@@ -107,7 +107,7 @@ export default function CohortsSection() {
                 </div>
                 <div className="absolute inset-0 bg-gradient-to-br from-primary/5 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
 
-                <div className="h-48 bg-gray-800 relative">
+                <div className="h-56 bg-gray-800 relative">
                   {playingVideo === cohort.id ? (
                     // YouTube iframe when playing
                     <div className="absolute inset-0 w-full h-full">
@@ -190,7 +190,7 @@ export default function CohortsSection() {
                       <img
                         src={`https://img.youtube.com/vi/${cohort.videoId}/mqdefault.jpg`}
                         alt={`${cohort.title} thumbnail`}
-                        className="w-full h-full object-cover"
+                        className="w-full h-full object-contain bg-black"
                       />
 
                       <div className="absolute inset-0 bg-black/30 flex items-center justify-center">
